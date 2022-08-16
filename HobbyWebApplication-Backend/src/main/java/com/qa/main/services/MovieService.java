4997ffb3b3b2b595d3617cb436d46db6fba9987d
dnmihnea <mihnea.dinu100@gmail.com>
2022-08-16 12:19:07 +0100

    added a few methods to MovieService, created exception class

--- HobbyWebApplication-Backend/src/main/java/com/qa/main/services/MovieService.java
+++ HobbyWebApplication-Backend/src/main/java/com/qa/main/services/MovieService.java
@@ -1,5 +1,32 @@
 package com.qa.main.services;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.qa.main.domain.Movie;
+import com.qa.main.exceptions.MovieNotFoundException;
+import com.qa.main.repos.MovieRepo;
+
+@Service
 public class MovieService {
+	
+	private MovieRepo repo;
+	
+	public MovieService(MovieRepo repo) {
+		this.repo = repo;
+	}
+	
+	public Movie create(Movie movie) {
+		return repo.saveAndFlush(movie);
+	}
+	
+	public List<Movie> getAll() {
+		return repo.findAll();
+	}
+	
+	public Movie getById(long id) {
+		return repo.findById(id).orElseThrow(MovieNotFoundException::new);
+	}
 
 }
