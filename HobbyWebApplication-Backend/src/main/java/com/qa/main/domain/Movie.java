b9c723bd160fd9d48b03e676ddc94849785f48ff
dnmihnea <mihnea.dinu100@gmail.com>
2022-08-16 11:36:20 +0100

    added getters, setters, hashcode, equals

--- HobbyWebApplication-Backend/src/main/java/com/qa/main/domain/Movie.java
+++ HobbyWebApplication-Backend/src/main/java/com/qa/main/domain/Movie.java
@@ -1,5 +1,7 @@
 package com.qa.main.domain;
 
+import java.util.Objects;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -42,6 +44,65 @@ public class Movie {
 		this.releaseYear = releaseYear;
 		this.director = director;
 	}
+
+	
+	//Getters and Setters 
+	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getReleaseYear() {
+		return releaseYear;
+	}
+
+	public void setReleaseYear(int releaseYear) {
+		this.releaseYear = releaseYear;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	public void setDirector(String director) {
+		this.director = director;
+	}
+
+	//HashCode and Equals methods
+	
+	@Override
+	public int hashCode() {
+		return Objects.hash(director, id, releaseYear, title);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Movie other = (Movie) obj;
+		return Objects.equals(director, other.director) && id == other.id && releaseYear == other.releaseYear
+				&& Objects.equals(title, other.title);
+	}
+	
+	
+	
+	
 	
 	
 
